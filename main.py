149ff617dc02504768d2b36369f9eed80ec2a36a
yahyacodes <yahya.codin@gmail.com>
2023-12-14 15:22:51 +0300

    Completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from typing import List
 
 app = FastAPI()
 
+# Movies pydantic BaseModel
 class MoviesModel(BaseModel):
     id :int
     title :str
@@ -13,37 +14,75 @@ class MoviesModel(BaseModel):
     director_id :int
     category :str
 
-@app.get('/')
-def home():
-    return {'data': 'Hello'}
+# Actors pydantic BaseModel
+class ActorsModel(BaseModel):
+    id :int
+    name :str
+    country :str
+    birth_date :str
 
+# Actors pydantic BaseModel
+class DirectorsModel(BaseModel):
+    id :int
+    name :str
+    country :str
+    birth_date :str
+
+# Fetching Actors on loading
 @app.get('/actors')
 def get_actors():
     actors = session.query(Actors).all()
     return actors
 
+# Fetching songle Actor
+@app.get('/actors/{id}')
+def single_actor(id:int) -> ActorsModel:
+    single_actor = session.query(Actors).filter_by(id=id).first()
+    return single_actor
+
+# Updating actors
+@app.patch('/actors/{id}')
+def update_actor(id:int, data:ActorsModel):
+    actor = session.query(Actors).filter_by(id=id).first()
+    for key, value in data.dict(exclude_unset=True).items():
+        setattr(actor, key, value)
+    session.commit()
+    return {'Actor updated successfully'}
+
+# Fetching directors on loading
 @app.get('/directors')
 def get_directors():
     directors = session.query(Directors).all()
     return directors
 
+# Fetching single director
+@app.get('/directors/{id}')
+def single_director(id:int) -> DirectorsModel:
+    single_director = session.query(Directors).filter_by(id=id).first()
+    return single_director
+
+# Updating directors
+@app.patch('/directors/{id}')
+def update_director(id:int, data:DirectorsModel):
+    director = session.query(Directors).filter_by(id=id).first()
+    for key, value in data.dict(exclude_unset=True).items():
+        setattr(director, key, value)
+    session.commit()
+    return {'Director updated successfully'}
+
+# Fetching movies on loading
 @app.get('/movies')
 def get_movies() -> List[MoviesModel]:
     movies = session.query(Movies).all()
     return movies
 
+# Fetching single movie
 @app.get('/movies/{id}')
 def single_movie(id:int) -> MoviesModel:
     single_movie = session.query(Movies).filter_by(id=id).first()
     return single_movie
 
-@app.delete('/movies/{id}')
-def delete_movie(id:int):
-    movie = session.query(Movies).filter_by(id=id).first()
-    session.delete(movie)
-    session.commit()
-    return {'One movie has been deleted successfully'}
-
+# Adding new movie
 @app.post('/movies')
 def add_new_movie(data:MoviesModel)-> MoviesModel:
     new_movie = Movies(**data.dict())
@@ -51,6 +90,7 @@ def add_new_movie(data:MoviesModel)-> MoviesModel:
     session.commit()
     return new_movie
 
+# Updating movies
 @app.patch('/movies/{id}')
 def update_movie(id:int, data:MoviesModel):
     movie = session.query(Movies).filter_by(id=id).first()
@@ -58,3 +98,11 @@ def update_movie(id:int, data:MoviesModel):
         setattr(movie, key, value)
     session.commit()
     return {'Movie updated successfully'}
+
+# Deleting movies
+@app.delete('/movies/{id}')
+def delete_movie(id:int):
+    movie = session.query(Movies).filter_by(id=id).first()
+    session.delete(movie)
+    session.commit()
+    return {'One movie has been deleted successfully'}
--- models.py
+++ models.py
@@ -1,8 +1,17 @@
-from sqlalchemy import Integer, Column, String, create_engine, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker, backref
+from sqlalchemy import Integer, Column, String, create_engine, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 Base = declarative_base()
 
+# Many to many relationship between actors and movies
+actor_movie_table = Table(
+    'actor_movie', 
+    Base.metadata,
+    Column('actor_id', Integer, ForeignKey('actors.id')),
+    Column('movie_id', Integer, ForeignKey('movies.id'))
+)
+
+# Creating Actors table
 class Actors(Base):
     __tablename__ = 'actors'
 
@@ -10,8 +19,9 @@ class Actors(Base):
     name = Column(String)
     birth_date = Column(Integer)
     country = Column(String)
-    movies = relationship("Movies", back_populates="actor")
+    movies = relationship("Movies", secondary=actor_movie_table, back_populates="actor")
 
+# Creating Movies table
 class Movies(Base):
     __tablename__ = 'movies'
     
@@ -22,8 +32,9 @@ class Movies(Base):
     category = Column(String)
     director_id = Column(Integer, ForeignKey('directors.id'))
     director = relationship("Directors", back_populates="movies")
-    actor = relationship("Actors", back_populates="movies")
+    actor = relationship("Actors", secondary=actor_movie_table, back_populates="movies")
 
+# Creating Directors table
 class Directors(Base):
     __tablename__ = 'directors'
     
@@ -33,7 +44,7 @@ class Directors(Base):
     country = Column(String)
     movies = relationship("Movies", back_populates="director")
 
-
+# Creating the database
 enigne = create_engine('sqlite:///movies.db')
 Session = sessionmaker(bind=enigne)
 session = Session()
